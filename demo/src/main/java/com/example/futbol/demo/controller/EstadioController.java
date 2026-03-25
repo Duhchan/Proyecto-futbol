9e25211d68e9a4694a02a4801d61b91aa3e4b2f5
Francisco <fra.olivaresp@duocuc.cl>
2026-03-25 01:14:56 -0300

    equipo controller reloaded

--- demo/src/main/java/com/example/futbol/demo/controller/EstadioController.java
+++ demo/src/main/java/com/example/futbol/demo/controller/EstadioController.java
@@ -1,4 +1,29 @@
 package com.example.futbol.demo.controller;
 
+import com.example.futbol.demo.model.Estadio;
+import com.example.futbol.demo.repositorio.EstadioInterface;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/estadios")
 public class EstadioController {
+
+    private final EstadioInterface estadioInterface;
+
+    public EstadioController(EstadioInterface estadioInterface) {
+        this.estadioInterface = estadioInterface;
+    }
+
+    @GetMapping("listarEstadios")
+    public ResponseEntity<?> listarEstadios() {
+        return ResponseEntity.ok(estadioInterface.findAll());
+    }
+
+    @PostMapping("addEstadio")
+    public ResponseEntity<?> addEstadio(@RequestBody Estadio nuevoEstadio) {
+        Estadio estadioSave = estadioInterface.save(nuevoEstadio);
+        return ResponseEntity.status(HttpStatus.CREATED).body(estadioSave);
+    }
 }
