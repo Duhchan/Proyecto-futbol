9ee14fc9248d948bf62cd87dbea246e9a2cd4a3b
Francisco <fra.olivaresp@duocuc.cl>
2026-03-25 00:32:21 -0300

    equipo controller

--- demo/src/main/java/com/example/futbol/demo/controller/EquipoController.java
+++ demo/src/main/java/com/example/futbol/demo/controller/EquipoController.java
@@ -1,5 +1,31 @@
 package com.example.futbol.demo.controller;
 
+import com.example.futbol.demo.model.Equipo;
+import com.example.futbol.demo.repositorio.EquipoInterface;
+import com.example.futbol.demo.repositorio.EstadioInterface;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequestMapping("/api/v1/equipos")
 public class EquipoController {
+
+    public final EquipoInterface equipoInterface;
+
+    public final EstadioInterface estadioInterface;
+
+    public EquipoController(EquipoInterface equipoInterface, EstadioInterface estadioInterface) {
+        this.equipoInterface = equipoInterface;
+        this.estadioInterface = estadioInterface;
+    }
+
+    @GetMapping("listarEquipos")
+    public ResponseEntity<?>listarEquipo(){
+        return ResponseEntity.ok(equipoInterface.findAll())
+    }
+
+    @PostMapping("addEquipo")
+    public ResponseEntity<?> addEquipo(@RequestBody Equipo nuevoEquipo){
+
+    }
 }
--- demo/src/main/java/com/example/futbol/demo/repositorio/EstadioInterface.java
+++ demo/src/main/java/com/example/futbol/demo/repositorio/EstadioInterface.java
@@ -1,4 +1,7 @@
 package com.example.futbol.demo.repositorio;
 
-public interface EstadioInterface {
+import com.example.futbol.demo.model.Estadio;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadioInterface extends JpaRepository<Estadio, Integer> {
 }
